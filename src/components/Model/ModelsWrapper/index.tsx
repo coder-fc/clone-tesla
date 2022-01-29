2aafd5b9dff24ca8acc67b56016d650899aa7e28
Felipe Costa Developer <felipech3344@gmail.com>
2022-01-29 16:24:53 -0300

    overlay_animation
    Codificação da animações dos overlays com useTransform utilizando calculos para mostrarmos o overlay apenas quando o usuário estiver em determinado momento na div de cada model.

--- src/components/Model/ModelsWrapper/index.tsx
+++ src/components/Model/ModelsWrapper/index.tsx
@@ -1,45 +1,56 @@
 import React, { useCallback, useRef, useState } from "react";
 import ModelsContext, { CarModel } from "../ModelsContext";
 
-import ModelOverlay from "../ModelOverlay"
+import ModelOverlay from "../ModelOverlay";
 
 import { Container, OverlaysRoot } from "./styles";
 
 const ModelsWrapper: React.FC = ({ children }) => {
-  const wrapperRef = useRef<HTMLDivElement>(null)
+  const wrapperRef = useRef<HTMLDivElement>(null);
 
-  const [registeredModels, setRegisteredModels] = useState<CarModel[]>([])
+  const [registeredModels, setRegisteredModels] = useState<CarModel[]>([]);
 
-  const registerModel = useCallback((model:CarModel) => {
-    setRegisteredModels(state => [...state, model])
-  }, [])
+  const registerModel = useCallback((model: CarModel) => {
+    setRegisteredModels((state) => [...state, model]);
+  }, []);
 
   const unregisterModel = useCallback((modelName: string) => {
-     setRegisteredModels(state => state.filter(model => model.modelName !== modelName))
-  }, [])
+    setRegisteredModels((state) =>
+      state.filter((model) => model.modelName !== modelName)
+    );
+  }, []);
 
-  const getModelByName = useCallback((modelName: string) => {
-    return registeredModels.find(item => item.modelName === modelName) || null
-  }, [registeredModels])
+  const getModelByName = useCallback(
+    (modelName: string) => {
+      return (
+        registeredModels.find((item) => item.modelName === modelName) || null
+      );
+    },
+    [registeredModels]
+  );
 
   return (
-    <ModelsContext.Provider value={{
-      wrapperRef,
-      registeredModels, 
-      registerModel, 
-      unregisterModel,
-      getModelByName
-    }}>
+    <ModelsContext.Provider
+      value={{
+        wrapperRef,
+        registeredModels,
+        registerModel,
+        unregisterModel,
+        getModelByName,
+      }}
+    >
       <Container ref={wrapperRef}>
         <OverlaysRoot>
-          {registeredModels.map(item => (
-            <ModelOverlay key={item.modelName}>{item.overlayNode}</ModelOverlay>
+          {registeredModels.map((item) => (
+            <ModelOverlay key={item.modelName} model={item}>
+              {item.overlayNode}
+            </ModelOverlay>
           ))}
         </OverlaysRoot>
         {children}
       </Container>
     </ModelsContext.Provider>
-  )
+  );
 };
 
 export default ModelsWrapper;
